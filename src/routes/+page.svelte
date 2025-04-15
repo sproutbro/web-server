d16894b18eaa2d5b98876d703c1714919eb291b8
Jackie <krsproutbro@google.com>
2025-04-15 11:28:03 +0900

    퀴즈 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,3 +13,19 @@
         />
     </div>
 </div>
+
+<div class="card">
+    <div class="links" style="justify-content: space-between;">
+        <div>
+            <a href="/game/quiz" class="write-button"> Start game </a>
+            <div>
+                <small>가로세로 낱말퀴즈</small>
+            </div>
+        </div>
+        <img
+            src="./quiz.png"
+            alt="quiz"
+            style="max-height:100px; max-width:50%;"
+        />
+    </div>
+</div>
